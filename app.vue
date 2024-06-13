962a9aca910b195fc73617027e21297ecbdb0d33
xiongliping <xiongliping181@163.com>
2024-06-12 20:44:01 -0700

    framework of frontend project is done

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <NuxtLink to="/"></NuxtLink>
+    <NuxtPage />
+  </div>
+</template>
---
+++ components/AppHeader.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+                <span class="me-4 md:me-6">Hi!</span>
+            </li>
+
+            <li>
+                <a href="#" class="hover:underline me-4 md:me-6">Login</a>
+            </li>
+            <li>
+                <span class="me-4 md:me-6">or</span>
+            </li>
+            <li>
+                <a href="#" class="hover:underline me-4 md:me-6">Register</a>
+            </li>
+        </ul>
+        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+                <a href="#" class="hover:underline me-4 md:me-6">Sell</a>
+            </li>
+            <li>
+                <span class="me-4 md:me-6">|</span>
+            </li>
+            <li>
+                <a href="#" class="hover:underline me-4 md:me-6">Watch List</a>
+            </li>
+            <li>
+                <span class="me-4 md:me-6">|</span>
+            </li>
+            <li>
+                <a href="#" class="hover:underline me-4 md:me-6">My account</a>
+            </li>
+
+        </ul>
+    </div>
+
+</template>
